22eb06a9011977cbdae5391e7fca2134d8c7d324
Abhishek Kumar Singh <141712253+abhishekksingh02@users.noreply.github.com>
2024-05-03 18:32:45 +0530

    Add files via upload

---
+++ CPP-Basics/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{   // cout
+    // cout << "Hello World!" << endl;
+
+    // //cerr
+    // cerr << "Used for error!!" << endl;
+
+    // //clog
+    // clog << "This is clog message!" << endl;
+
+    // cin
+    string name;
+    cout << "Enter your name." << endl;
+    cin >> name ;
+    cout << "Your name is: " << name << endl;
+
+
+    return 0;
+}
