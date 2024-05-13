25f9ecf5b33645c7700b6f69e90611049275a4e5
Abhishek Kumar Singh <141712253+abhishekksingh02@users.noreply.github.com>
2024-05-13 11:14:35 +0530

    Add files via upload

---
+++ CPP-GUI/QSettingsDemo/widget.h
@@ -0,0 +1,58 @@
+#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+#include <QList>
+#include <QColor>
+#include <QPushButton>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class Widget;
+}
+QT_END_NAMESPACE
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    Widget(QWidget *parent = nullptr);
+    ~Widget();
+
+private slots:
+    void on_button1_clicked();
+
+    void on_button2_clicked();
+
+    void on_button3_clicked();
+
+    void on_button4_clicked();
+
+    void on_button5_clicked();
+
+    void on_button6_clicked();
+
+    void on_button7_clicked();
+
+    void on_button8_clicked();
+
+    void on_button9_clicked();
+
+    void on_loadPushButton_clicked();
+
+    void on_savePushButton_clicked();
+
+private:
+    Ui::Widget *ui;
+
+    QList<QColor> colorList;
+
+    //The method to save the colors
+    void saveColor(QString key, QColor color);
+    QColor loadColor(QString key);
+
+    void setLoadedColor(QString key, int index, QPushButton * button );
+
+};
+#endif // WIDGET_H
